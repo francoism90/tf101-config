40df92cd945d55d6f1352e61b9582b5902d1e48f
Francois <francoism90@users.noreply.github.com>
2015-01-11 14:03:52 +0100

    Update reflash_sbkv2.sh

--- reflash_sbkv2.sh
+++ reflash_sbkv2.sh
@@ -3,5 +3,5 @@ ISINCLUDED=1
 . ./checkfiles.sh
 
 ./wheelie -2 --bl bootloader.bin --bct transformer.bct --odm 0x300d8011
-./nvflash -r --bct transformer.bct --setbct --configfile flash.cfg --create --odmdata 0x300d8011 --sync
+./nvflash -r --bct transformer.bct --setbct --configfile flash.cfg --create --odmdata 0x300d8011
 ./nvflash -r --go
